7832e4e0d379dc72dbecc3ffb65bc30129597b0f
WatermelonMojito <WatermelonMojitoYT@gmail.com>
2023-12-14 20:31:56 -0600

    All references to ExampleMod changed to Elevators

--- src/main/java/melonmojito/elevatorsmod/ElevatorsMod.java
+++ src/main/java/melonmojito/elevatorsmod/ElevatorsMod.java
@@ -1,16 +1,16 @@
-package turniplabs.examplemod;
+package melonmojito.elevatorsmod;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class ExampleMod implements ModInitializer {
-    public static final String MOD_ID = "examplemod";
+public class ElevatorsMod implements ModInitializer {
+    public static final String MOD_ID = "elevatorsmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        LOGGER.info("ExampleMod initialized.");
+        LOGGER.info("ElevatorsMod initialized.");
     }
 }
